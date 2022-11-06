c2f28eba6ed5505073be9ad37412f3addf3c4f7e
Dustin Cook <dustin@libglug.org>
2022-11-06 10:51:17 -0800

    add float::min, float::max, float::clamp (#8)

--- glug_math/src/float.c
+++ glug_math/src/float.c
@@ -1,6 +1,16 @@
 #include <glug/math/float.h>
 #include <math.h>
 
+float glug_float_next(float f)
+{
+    return nextafterf(f, INFINITY);
+}
+
+float glug_float_prev(float f)
+{
+    return nextafterf(f, -INFINITY);
+}
+
 void glug_float_swap(float *f1, float *f2)
 {
     float tmp = *f1;
@@ -16,14 +26,19 @@ float glug_float_sign(float f)
     return pos - neg;
 }
 
-float glug_float_next(float f)
+float glug_float_min(float f1, float f2)
 {
-    return nextafterf(f, INFINITY);
+    return (f1 < f2) * f1 + (f1 >= f2) * f2;
 }
 
-float glug_float_prev(float f)
+float glug_float_max(float f1, float f2)
 {
-    return nextafterf(f, -INFINITY);
+    return (f1 > f2) * f1 + (f1 <= f2) * f2;
+}
+
+float glug_float_clamp(float f, float min, float max)
+{
+    return glug_float_max(min, glug_float_min(f, max));
 }
 
 glug_bool_t glug_float_equal_strict(float f1, float f2)
@@ -38,11 +53,11 @@ glug_bool_t glug_float_equal_approx(float f1, float f2, float diff)
 
 glug_bool_t glug_float_equal_ulps(float f1, float f2, uint32_t ulps)
 {
-    float smaller = (f1 < f2) * f1 + (f2 <= f1) * f2;
-    float larger  = (f1 > f2) * f1 + (f2 >= f1) * f2;
+    float min = glug_float_min(f1, f2);
+    float max = glug_float_max(f1, f2);
 
-    for (; ulps-- && smaller <= larger;)
-        smaller = nextafterf(smaller, INFINITY);
+    for (; ulps-- && min <= max;)
+        min = glug_float_next(min);
 
-    return larger <= smaller;
+    return max <= min;
 }
